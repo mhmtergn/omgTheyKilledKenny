6156380335638896aedc52a76b6bb85c1f5bd642
Mehmet Ergun <mrmehmetergun@gmail.com>
2022-05-10 02:07:39 +0300

    Time Counter, Score, Alert Buttons added

--- omgTheyKilledKenny/ViewController.swift
+++ omgTheyKilledKenny/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // variables
+    
+    var score = 0
+    var timer = Timer()
+    var timeCounter = 0
+    
     //Labels
     
     @IBOutlet weak var timeLabel: UILabel!
@@ -31,12 +37,78 @@ class ViewController: UIViewController {
     
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Recognizers
+        
+        kenny1.isUserInteractionEnabled = true
+        kenny2.isUserInteractionEnabled = true
+        kenny3.isUserInteractionEnabled = true
+        kenny4.isUserInteractionEnabled = true
+        kenny5.isUserInteractionEnabled = true
+        kenny6.isUserInteractionEnabled = true
+        kenny7.isUserInteractionEnabled = true
+        kenny8.isUserInteractionEnabled = true
+        kenny9.isUserInteractionEnabled = true
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        
+        kenny1.addGestureRecognizer(recognizer1)
+        kenny2.addGestureRecognizer(recognizer2)
+        kenny3.addGestureRecognizer(recognizer3)
+        kenny4.addGestureRecognizer(recognizer4)
+        kenny5.addGestureRecognizer(recognizer5)
+        kenny6.addGestureRecognizer(recognizer6)
+        kenny7.addGestureRecognizer(recognizer7)
+        kenny8.addGestureRecognizer(recognizer8)
+        kenny9.addGestureRecognizer(recognizer9)
+        
+        // Timer
+        
+        timeCounter = 10
+        timeLabel.text = String(timeCounter)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func increaseScore() {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    @objc func countDown() {
+        timeCounter -= 1
+        timeLabel.text = String(timeCounter)
+        
+        if timeCounter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time's Up"
+            
+            // Alert
+            
+            let alert = UIAlertController(title: "Time's Up", message: "One More Round?", preferredStyle: UIAlertController.Style.alert)
+                       
+                       let okButton = UIAlertAction(title: "Nope", style: UIAlertAction.Style.cancel, handler: nil)
+                       
+                       let replayButton = UIAlertAction(title: "Hell Yeah!", style: UIAlertAction.Style.default) { UIAlertAction in
+                           //replay Function
+        }
+            alert.addAction(okButton)
+                       alert.addAction(replayButton)
+                       self.present(alert, animated: true, completion: nil)
     }
 
 
 }
 
+}
